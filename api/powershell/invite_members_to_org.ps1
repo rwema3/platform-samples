acb192bac927ea5c0e49ea8a8984375ec7176953
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-27 01:28:41 +0200

    Cloud consumed licenses

--- api/powershell/invite_members_to_org.ps1
+++ api/powershell/invite_members_to_org.ps1
@@ -6,3 +6,6 @@ Batch invite members to an organization
 
 .DESCRIPTION
 This script runs a batch organization invitation process for a given list of GitHub Enterprise
+Cloud consumed licenses.
+
+The input is a CSV file with a column named "Handle or email", such as can be exported from the
