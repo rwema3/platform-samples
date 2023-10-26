e04b03d0c1a6fc7ad53190b57b088fa28243f858
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-27 01:29:00 +0200

    spreedsheet

--- api/powershell/invite_members_to_org.ps1
+++ api/powershell/invite_members_to_org.ps1
@@ -10,3 +10,6 @@ Cloud consumed licenses.
 
 The input is a CSV file with a column named "Handle or email", such as can be exported from the
 Enterprise settings > Enterprise licensing page. Users with appropriate permissions can export
+the CSV file, edit it in their favorite spreadsheet to select emails to invite, then use this
+script to invite them to an org.
+
