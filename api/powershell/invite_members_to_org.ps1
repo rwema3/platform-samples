3d533814d55225be7a34512b55aa48a2d26420e5
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-27 01:28:49 +0200

    Enterprise settings

--- api/powershell/invite_members_to_org.ps1
+++ api/powershell/invite_members_to_org.ps1
@@ -9,3 +9,4 @@ This script runs a batch organization invitation process for a given list of Git
 Cloud consumed licenses.
 
 The input is a CSV file with a column named "Handle or email", such as can be exported from the
+Enterprise settings > Enterprise licensing page. Users with appropriate permissions can export
