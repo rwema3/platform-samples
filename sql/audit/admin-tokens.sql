c30d24adb8891c8daaf9af82d3d8abf8e46e045a
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-28 23:39:23 +0200

    This pulls a list of all apps, tokens, and scopes associated with that token

---
+++ sql/audit/admin-tokens.sql
@@ -0,0 +1,5 @@
+/*
+ * This pulls a list of all apps, tokens, and scopes associated with that token
+ * as well as when it was last used, created, and updated for anything with 
+ * the `site_admin` scope.
+ */
