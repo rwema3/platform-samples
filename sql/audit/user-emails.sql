dd70a79fe9de3150030e7d47ff9ce3717d3c9a5f
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-28 23:43:23 +0200

    user login

---
+++ sql/audit/user-emails.sql
@@ -0,0 +1,14 @@
+/* 
+ * This pulls a list of all email addresses and the user account it is tied to
+ * that don't match the list of domains in the WHERE clause.  Add however many
+ * "%domain.com" needed to cover your company's approved domains.
+ *
+ * This query should be deprecated by this issue:
+ * https://github.com/github/roadmap/issues/204
+ *
+ * If you want a list of all emails, remove the WHERE clause.
+ */
+SELECT
+	u.login,
+	e.email,
+	u.suspended_at
