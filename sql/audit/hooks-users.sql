b71aad135b61a2e7566f1caf746cc8a72fbe4b1f
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-28 23:40:45 +0200

    selection and distinct

--- sql/audit/hooks-users.sql
+++ sql/audit/hooks-users.sql
@@ -3,3 +3,12 @@
  * week, who owns them, and where the webhook goes.
  */
 SELECT
+	DISTINCT h.id,
+	u.login as creator,
+	h.updated_at,
+	u.login as repo_owner,
+	u.type as owner_type,
+	c.value as url,
+	MAX(l.delivered_at) as latest_delivery
+FROM
+	github_enterprise.hooks h
