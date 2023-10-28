7333f9f836c808db6d7d9cfef65925ac22938282
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-28 23:39:40 +0200

    selection based on attributes

--- sql/audit/admin-tokens.sql
+++ sql/audit/admin-tokens.sql
@@ -3,3 +3,10 @@
  * as well as when it was last used, created, and updated for anything with 
  * the `site_admin` scope.
  */
+SELECT
+	z.id,
+	u.login as owner_name,
+	u.type as owner_type,
+	a.name as app_name,
+	z.accessed_at,
+	z.created_at,
